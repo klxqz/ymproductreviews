e03b7c9f753804328329abc27cffe4c0f8382169
klxqz <klxqz@mail.ru>
2013-11-22 15:27:52 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,18 +3,17 @@
 return array(
     'name' => 'Отзывы о товаре с Яндекс.Маркета',
     'description' => 'Вывод отзывов в карточке товара',
-    'vendor'=>903438,
-    'version'=>'1.0.1',
-    'img'=>'img/ymproductreviews.png',
+    'vendor' => '985310',
+    'version' => '1.0.2',
+    'img' => 'img/ymproductreviews.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/ymproductreviews.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/ymproductreviews.png',
     ),
     'handlers' => array(
         'backend_product_edit' => 'backendProductEdit',
         'frontend_product' => 'frontendProduct',
     ),
-
 );
 //EOF
